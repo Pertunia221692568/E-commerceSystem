1ebcaa7c053a1e9755cc5586d5d80647a6083ea3
aneeqah talaaboedien <219099405@mycput.ac.za>
2026-03-27 20:41:48 +0200

    Revert "Merge pull request #22 from Ntlantla-cput/Ntlantla-cput-222133872"
    This reverts commit 3aefb4ff78ef925e2c840327bf682df1d29595d4, reversing
    changes made to 4b5d7d302ef8f18b85b0d2f0ccf77c829a76072d.

--- src/main/java/za/ac/cput/domain/Product.java
+++ src/main/java/za/ac/cput/domain/Product.java
@@ -1,21 +1,5 @@
 package za.ac.cput.domain;
 
- Ntlantla-cput-222133872
-import java.util.Objects;
-
-/**
- * Product.java
- * Product model class using Builder Pattern
- * Author: Sinentlantla Slayi (222133872)
- * Date: 27 March 2026
- */
-public class Product {
-    private String productId;
-    private String productName;
-    private double price;
-
-    private Product() {}
-
 public class Product {
 
     private String productId;
@@ -25,18 +9,12 @@ public class Product {
     private Product() {
 
     }
-master
 
     private Product(Builder builder) {
         this.productId = builder.productId;
         this.productName = builder.productName;
         this.price = builder.price;
     }
-
- Ntlantla-cput-222133872
-    public String getProductId() { return productId; }
-    public String getProductName() { return productName; }
-    public double getPrice() { return price; }
 
     public String getProductId() {
         return productId;
@@ -58,7 +36,6 @@ master
                 ", price=" + price +
                 '}';
     }
-master
 
     public static class Builder {
         private String productId;
@@ -80,26 +57,8 @@ master
             return this;
         }
 
- Ntlantla-cput-222133872
-        public Builder copy(Product product) {
-            this.productId = product.productId;
-            this.productName = product.productName;
-            this.price = product.price;
-            return this;
-        }
-
-
-master
         public Product build() {
             return new Product(this);
         }
     }
- Ntlantla-cput-222133872
-
-    @Override
-    public String toString() {
-        return "Product{" + "productId='" + productId + '\'' + ", productName='" + productName + '\'' + ", price=" + price + '}';
-    }
-
- master
 }
